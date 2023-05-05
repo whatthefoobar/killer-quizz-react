06c87d71a184f66b65a9aeb5dde7435e2b733faa
EliasFredriksson <elias.e.fredriksson@gmail.com>
2023-05-05 17:40:56 +0200

    updated

--- src/util/formReducer.js
+++ src/util/formReducer.js
@@ -27,43 +27,13 @@ export const formInputReducer = (state, action) => {
     case "GET_ANSWER":
       const stateFn = mapAnswerTypeToStateFn[action.id];
       return stateFn ? stateFn(state) : state;
-    // {
-    //   // let answerType = action.id;
-    //   // if (answerType === "a1") {
-    //   //   let prevState = state.score;
-    //   //   let newScore = prevState + 1;
-    //   //   return {
-    //   //     ...state,
-    //   //     score: newScore,
-    //   //   };
-    //   // } else if (answerType === "a2") {
-    //   //   let prevState = state.score;
-    //   //   let newScore = prevState + 2;
-    //   //   return {
-    //   //     ...state,
-    //   //     score: newScore,
-    //   //   };
-    //   // } else if (answerType === "a3") {
-    //   //   let prevState = state.score;
-    //   //   let newScore = prevState + 3;
-    //   //   return {
-    //   //     ...state,
-    //   //     score: newScore,
-    //   //   };
-    //   // } else {
-    //   //   let prevState = state.score;
-    //   //   let newScore = prevState + 4;
-    //   //   return {
-    //   //     ...state,
-    //   //     score: newScore,
-    //   //   };
-    //   // }
-    // }
+
     case "GET_NAME":
       return {
         ...state,
         name: action.name,
       };
+
     default:
       return state;
   }
