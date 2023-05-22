40ce64ab40aa16b2fc5039b41e0547c66da59f1e
whatthefoobar <irina.mntn@gmail.com>
2023-05-22 13:27:57 +0200

    qna data is grabbed from fb in the exact obj order i created it so no errors in my code

--- frontend/src/pages/Questionnaire.js
+++ frontend/src/pages/Questionnaire.js
@@ -1,6 +1,7 @@
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import "./Questionnaire.css";
-import { questions } from "../util/data";
+import axios from "axios";
+// import { questions } from "../util/data";
 
 import { initialState, formReducer } from "../util/formReducer";
 
@@ -8,6 +9,34 @@ const Questionnaire = () => {
   const [state, dispatch] = useReducer(formReducer, initialState);
   const { currentQuestionIndex, answers } = state;
   const [showResult, setShowResult] = useState(false);
+  const [questions, setQuestions] = useState([]);
+
+  const removeIdsFromArray = (array) => {
+    // Extract the values from the object and remove the IDs
+    const newArray = Object.values(array).map((obj) => {
+      const { id, ...rest } = obj;
+      return rest;
+    });
+
+    return newArray;
+  };
+
+  // now grab questions data not from util but from FB here
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/data") // Replace with your server endpoint URL
+      .then((response) => {
+        setQuestions(removeIdsFromArray(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log(questions);
+  }, []);
+
+  useEffect(() => {
+    console.log(questions);
+  }, [questions]);
 
   const handleAnswerSelect = (answer) => {
     dispatch({ type: "SELECT_ANSWER", payload: answer });
@@ -48,53 +77,55 @@ const Questionnaire = () => {
 
   return (
     <div className="questionnaire">
-      <form onSubmit={handleSubmit}>
-        <div className="questionnaire--qna">
-          <h2>{currentQuestion.question}</h2>
-          {!currentQuestion.input ? (
-            currentQuestion.options.map((option) => (
-              <div key={option.value}>
-                <label htmlFor={option.value}>
-                  <input
-                    type="radio"
-                    id={option.value}
-                    name="answer"
-                    value={option.value}
-                    checked={answers[currentQuestionIndex] === option.value}
-                    onChange={() => handleAnswerSelect(option.value)}
-                  />
-                  {option.label}
-                </label>
-              </div>
-            ))
-          ) : (
-            <label htmlFor="name">
-              <input
-                id="name"
-                type="text"
-                minLength="4"
-                maxLength="12"
-                value={state.name}
-                onChange={handleNameChange}
-              />
-            </label>
-          )}
-        </div>
-        <div className="questionnaire--buttons">
-          {currentQuestionIndex > 0 && (
-            <button type="button" onClick={handlePrev}>
-              Previous
-            </button>
-          )}
-          {currentQuestionIndex < questions.length - 1 ? (
-            <button type="button" onClick={handleNext}>
-              Next
-            </button>
-          ) : (
-            <button type="submit">Submit</button>
-          )}
-        </div>
-      </form>
+      {questions.length > 0 && (
+        <form onSubmit={handleSubmit}>
+          <div className="questionnaire--qna">
+            <h2>{currentQuestion.question}</h2>
+            {!currentQuestion.input ? (
+              currentQuestion.options.map((option) => (
+                <div key={option.value}>
+                  <label htmlFor={option.value}>
+                    <input
+                      type="radio"
+                      id={option.value}
+                      name="answer"
+                      value={option.value}
+                      checked={answers[currentQuestionIndex] === option.value}
+                      onChange={() => handleAnswerSelect(option.value)}
+                    />
+                    {option.label}
+                  </label>
+                </div>
+              ))
+            ) : (
+              <label htmlFor="name">
+                <input
+                  id="name"
+                  type="text"
+                  minLength="4"
+                  maxLength="12"
+                  value={state.name}
+                  onChange={handleNameChange}
+                />
+              </label>
+            )}
+          </div>
+          <div className="questionnaire--buttons">
+            {currentQuestionIndex > 0 && (
+              <button type="button" onClick={handlePrev}>
+                Previous
+              </button>
+            )}
+            {currentQuestionIndex < questions.length - 1 ? (
+              <button type="button" onClick={handleNext}>
+                Next
+              </button>
+            ) : (
+              <button type="submit">Submit</button>
+            )}
+          </div>
+        </form>
+      )}
 
       {showResult && result}
     </div>
