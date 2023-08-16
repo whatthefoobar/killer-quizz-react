500fdeed62247864975ec0faa253bec2e1b20c80
whatthefoobar <irina.mntn@gmail.com>
2023-08-16 09:06:18 +0200

    intro page styles change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,13 @@ function App() {
   };
   const intro = (
     <div className="start-page">
+      <h3>
+        We all have bad days and problems and most of us are good deep down
+        inside, however all of us have the potential to snap at any times if not
+        held in check.Here is a test for you to see how much you have in comon
+        with popular coulture serial killers from media and real life.
+      </h3>
+
       <h1>What serial killer would you be? take the test and find out</h1>
 
       <button onClick={startQuestionnaire}>Start</button>
